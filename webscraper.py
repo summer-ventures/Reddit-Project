ab3ab6d5c44ea927008b968e377388ffd1018466
ayxliu19 <40506840+ayxliu19@users.noreply.github.com>
2020-06-20 16:33:53 -0400

    Created Subreddit class that takes in subreddit,runtime,and interval as args

--- webscraper.py
+++ webscraper.py
@@ -5,29 +5,35 @@ import requests
 import re
 
 headers = {'user-agent': 'reddit-{}'.format(os.environ.get('USER'))}
+subreddit_name = 'movies'
 
-def number_active_users(subreddit):
+class Subreddit:
 	'''
-	Returns number of active users in a subreddit
-	Args: Subreddit
+	Subreddit class	that finds the number of active users in a given subreddit over a given timeperiod
 	'''
+	def __init__(self, subreddit, runtime, interval):
+		self.subreddit = subreddit
+		self.runtime = runtime
+		self.interval = interval
 
-	website = "https://www.reddit.com/r/"
+	def number_active_users(self):
+		'''
+		Returns number of active users in a subreddit
+		Args: Subreddit
+		'''
 
-	response = requests.get(website + subreddit, headers=headers)
+		website = "https://www.reddit.com/r/"
 
-	currently_viewing = re.search(r'(?<=\"currentlyViewingCount\":)\d+', response.text)
-	currently_viewing = int(currently_viewing.group(0))
+		response = requests.get(website + self.subreddit, headers=headers)
 
-	print(subreddit)
-	print(currently_viewing)
+		currently_viewing = re.search(r'(?<=\"currentlyViewingCount\":)\d+', response.text)
+		currently_viewing = int(currently_viewing.group(0))
+
+		print(self.subreddit)
+		print(currently_viewing)
 		
-	return currently_viewing
+		return currently_viewing
 
-if __name__ == "__main__":
-	number_active_users("movies")
-
-'''
-BLM
-
-'''
+if __name__ == '__main__':
+	sub = Subreddit(subreddit_name, 20, 60)
+	sub.number_active_users()
